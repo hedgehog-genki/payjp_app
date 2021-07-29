dc7a72959209f74c615a95c2564b061c1e738bc3
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-07-29 12:29:32 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  get "users/show"
   get "items/index"
   devise_for :users
   root "items#index"
 
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
 end
